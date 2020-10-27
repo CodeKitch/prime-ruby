3cfbe6056ba07ac7cb7d946efe55cae007337919
JaSON <masteraccounthub@gmail.com>
2020-10-27 09:21:30 -0400

    commit tree 7411b5e97577261ddf94b2eb069c71c8784d6da7

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(n)
+    if n <= 1
+      return false
+    else 
+        if (2...n).any? { |p| n % p  == 0}
+            return false
+        else
+            return true
+        end 
+    end
+end
